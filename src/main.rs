0d753e3fb529e772066d69fe7b560c5aeca5bf49
Kostas Kokkoros <mavroprovato@gmail.com>
2019-11-06 00:25:11 +0200

    Add skeleton for union find implementation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+#[derive(Debug)]
+struct UnionFind {
+
+}
+
+impl UnionFind {
+    fn new(size: u32) -> UnionFind {
+        UnionFind {}
+    }
+
+    fn union(self, p: u32, q: u32) {
+
+    }
+
+    fn find(self, p: u32) -> u32 {
+        0
+    }
+
+    fn connected(self, p: u32, q: u32) -> bool {
+        false
+    }
+
+    fn components(self) -> u32 {
+        0
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let uf = UnionFind::new(2);
+    println!("{:?}", uf)
 }
