cf6d461e8909f1a93af416685cfc76b4bae5c053
Kostas Kokkoros <mavroprovato@gmail.com>
2019-12-14 01:30:01 +0200

    Percolation: parse file and create structure

--- src/bin/percolation.rs
+++ src/bin/percolation.rs
@@ -1,25 +1,51 @@
 use std::env;
-use std::fs;
-use std::io::{BufRead, BufReader};
+use std::fs::File;
+use std::io::BufReader;
+use std::io::prelude::*;
+
+use rust_algorithms::union_find::UnionFind;
+use rust_algorithms::union_find::quick_find::UnionFindQuickFind;
 
 fn main() {
     // Parse arguments
-    let args: Vec<_> = env::args().collect();
+    let args: Vec<String> = env::args().collect();
     if args.len() < 2 {
         panic!("Input file not provided")
     }
 
-    // Read input file line by line
-    let file = match fs::File::open(&args[1]) {
+    // Open the input file
+    let file = match File::open(&args[1]) {
         Ok(v) => v,
-        Err(e) => panic!("Could not open file."),
+        Err(e) => panic!("Cannot open input file: {:?}", e),
     };
     let reader = BufReader::new(file);
+    let mut lines = reader.lines();
 
-    // Read the file line by line.
-    for (index, line) in reader.lines().enumerate() {
-        let line = line.unwrap(); // Ignore errors.
-        // Show the line and its number.
-        println!("{}. {}", index + 1, line);
+    // Create the union find structure
+    let size = match lines.next() {
+        Some(v) => match v.unwrap().parse::<usize>() {
+            Ok(v) => v,
+            Err(e) => panic!("Cannot parse first line as an integer: {:?}", e),
+        },
+        None => panic!("File is empty"),
+    };
+
+    // Read the file line by line
+    let mut uf = UnionFindQuickFind::new(size);
+    for line in lines {
+        let components: Vec<usize> = line.unwrap().split_whitespace().map(
+            |s| {
+                match s.parse::<usize>() {
+                    Ok(v) => v,
+                    Err(e) => panic!("Cannot parse component as an integer: {:?}", e),
+                }
+            }
+        ).collect();
+        if args.len() < 2 {
+            panic!("Cannot parse line {}: need at least two integers")
+        }
+        uf.union(components[0], components[1])
     }
+
+    println!("{:?}", uf);
 }
