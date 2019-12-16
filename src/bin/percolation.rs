50ec742558eb7a99a639fab23e98b2432c40e681
Kostas Kokkoros <mavroprovato@gmail.com>
2019-12-17 01:29:44 +0200

    Percolation: implement check for percolation

--- src/bin/percolation.rs
+++ src/bin/percolation.rs
@@ -4,7 +4,77 @@ use std::io::BufReader;
 use std::io::prelude::*;
 
 use rust_algorithms::union_find::UnionFind;
-use rust_algorithms::union_find::quick_find::UnionFindQuickFind;
+use rust_algorithms::union_find::quick_union::UnionFindQuickUnion;
+
+#[derive(Debug)]
+struct Percolation {
+    rows: usize,
+    columns: usize,
+    sites: Vec<bool>,
+    percolation_uf: UnionFindQuickUnion,
+}
+
+impl Percolation {
+    fn new(rows: usize, columns: usize) -> Self {
+        if rows == 0 || columns == 0 {
+            panic!("Dimensions should be greater that zero");
+        }
+
+        Self {
+            rows,
+            columns,
+            sites: vec![false; rows * columns],
+            percolation_uf: UnionFindQuickUnion::new(rows * columns + 2),
+        }
+    }
+
+    fn index(&self, row: usize, column: usize) -> usize {
+        if row >= self.rows || column > self.columns {
+            panic!("The cell to open is out of bounds: {} {}", row, column);
+        }
+
+        row * self.rows + column
+    }
+
+    fn open(&mut self, row: usize, column: usize) {
+        // Mark the site as open
+        let index = self.index(row, column);
+        self.sites[index] = true;
+
+        // Connect the site to adjacent sites
+        if row > 1 && self.is_open(row - 1, column) {
+            self.percolation_uf.union(index, self.index(row - 1, column));
+        }
+        if row < self.rows - 1 && self.is_open(row + 1, column) {
+            self.percolation_uf.union(index, self.index(row + 1, column));
+        }
+        if column > 1 && self.is_open(row, column - 1) {
+            self.percolation_uf.union(index, self.index(row, column - 1));
+        }
+        if column < self.columns - 1 && self.is_open(row, column + 1) {
+            self.percolation_uf.union(index, self.index(row, column + 1));
+        }
+
+        // Connect with the 'dummy' top site
+        if row == 0 {
+            self.percolation_uf.union(0, index);
+        }
+        // Connect with the 'dummy' bottom site
+        if row == self.rows - 1 {
+            self.percolation_uf.union(self.rows * self.columns + 1, index);
+        }
+    }
+
+    fn is_open(&self, row: usize, column: usize) -> bool {
+        let index = self.index(row, column);
+
+        self.sites[index]
+    }
+
+    fn percolates(&self) -> bool {
+        self.percolation_uf.connected(0, self.columns * self.rows + 1)
+    }
+}
 
 fn main() {
     // Parse arguments
@@ -18,17 +88,17 @@ fn main() {
         Ok(v) => v,
         Err(e) => panic!("Cannot open input file: {:?}", e),
     };
-    let uf = read_union_find(file);
+    let percolation = read_percolation_file(file);
 
-    println!("{:?}", uf);
+    println!("Percolates: {:?}", percolation.percolates());
 }
 
-fn read_union_find(file: File) -> UnionFindQuickFind {
+fn read_percolation_file(file: File) -> Percolation {
     // Open the input file
     let reader = BufReader::new(file);
     let mut lines = reader.lines();
 
-    // Create the union find structure
+    // Read the size of the grid
     let size = match lines.next() {
         Some(v) => match v.unwrap().parse::<usize>() {
             Ok(v) => v,
@@ -37,10 +107,11 @@ fn read_union_find(file: File) -> UnionFindQuickFind {
         None => panic!("File is empty"),
     };
 
-    // Read the file line by line and connect components
-    let mut uf = UnionFindQuickFind::new(size);
+    // Create the supporting percolation data structure
+    let mut percolation = Percolation::new(size, size);
     for line in lines {
-        let components: Vec<usize> = line.unwrap().split_whitespace().map(
+        // Split lines as whitespace separated integers
+        let dimension: Vec<usize> = line.unwrap().split_whitespace().map(
             |s| {
                 match s.parse::<usize>() {
                     Ok(v) => v,
@@ -48,11 +119,13 @@ fn read_union_find(file: File) -> UnionFindQuickFind {
                 }
             }
         ).collect();
-        if components.len() < 2 {
+        // Check if we got at least 2
+        if dimension.len() < 2 {
             panic!("Cannot parse line {}: need at least two integers")
         }
-        uf.union(components[0], components[1])
+        // Open site
+        percolation.open(dimension[0], dimension[1])
     }
 
-    uf
+    percolation
 }
